5d441feaca7b51aab0c45b2ea30eee4a9829cc24
Tim Conrad <tim@timconrad.org>
2012-06-18 13:39:27 -0400

    Moved fake carbon into it's own module. It's not currently a part of the overall build, but will be.

--- FakeCarbon/src/main/java/org/timconrad/fakecarbon/FakeCarbonPipelineFactory.java
+++ FakeCarbon/src/main/java/org/timconrad/fakecarbon/FakeCarbonPipelineFactory.java
@@ -1,4 +1,19 @@
 package org.timconrad.fakecarbon;
+/*
+ * Copyright 2012 Tim Conrad - tim@timconrad.org
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 
 import static org.jboss.netty.channel.Channels.*;
 
@@ -21,7 +36,7 @@ public class FakeCarbonPipelineFactory implements ChannelPipelineFactory {
 
     public ChannelPipeline getPipeline() throws Exception {
         // this is pretty verbose from the netty examples
-        ChannelPipeline pipeline = pipeline();
+        ChannelPipeline pipeline = Channels.pipeline();
         pipeline.addLast("framer", new DelimiterBasedFrameDecoder(8192, Delimiters.lineDelimiter()));
         pipeline.addLast("decoder", new StringDecoder());
         pipeline.addLast("encoder", new StringEncoder());
---
+++ tools/README.txt
@@ -0,0 +1,3 @@
+FakeCarbon -
+    This is a simple tool to emulate what the part of Graphite that actually gets the stats does. It has flags to
+    only display packets with errors and so on. It only captures stuff to logs at this point.
